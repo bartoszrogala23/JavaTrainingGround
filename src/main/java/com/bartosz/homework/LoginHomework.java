7f4a3ed5f1f598300287561c1d116241242c89f8
Bartosz Rogala <bartoszrogala23@gmail.com>
2020-10-30 19:27:55 +0100

    zadanie domowe nr 1

---
+++ src/main/java/com/bartosz/homework/LoginHomework.java
@@ -0,0 +1,11 @@
+package com.bartosz.homework;
+/*
+korzystajac z obecnej wiedzy (logika + pętle)
+stwórz program do logowania sie uzytkownika
+wymagania:
+1. program musi konczyc sie po okreslonej liczbie prob przez uzytkownika
+2. przy blednie wpisanym hasle powinna byc wypisana pozostala liczba prob oraz ostrzezenie
+3. w przypadku sukcesu powinno zostac wypisane odpowiedni komunikat
+ */
+public class LoginHomework {
+}
