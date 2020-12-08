08493cde016478a7d12cc2eb6ed1a295f352de37
Bartosz Rogala <bartoszrogala23@gmail.com>
2020-12-08 19:41:16 +0100

    zadanie pierwsze, podejście drugie - brak wyjątku jeśli jako ID podamy coś innego niż int.

--- src/main/java/com/bartosz/homework/homework02122020/Zad1.java
+++ src/main/java/com/bartosz/homework/homework02122020/Zad1.java
@@ -1,32 +1,40 @@
 package com.bartosz.homework.homework02122020;
 
-
-
-
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Scanner;
 
+//Wymagania:
+//        Napisz program, który wczytuje od użytkownika stringi w postaci klucz - wartość
+//        Dodaj je do słownika.
+//        Jeśli dany klucz istnieje w słowniku - nie rób nic.
+//
+//        Zapewnij możliwość podania kolejnych par klucz-wartość lub zaprzestawania ich podawania.
+//
+//        Wypisz elementy słownika na ekran w formie "klucz -> wartość"
+
 public class Zad1 {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         scanner.useLocale(Locale.US);
         int id;
         String name;
         boolean finisher = true;
+
         HashMap<Integer, String> map = new HashMap<>();
 
-        while (true){
-            System.out.println("what would You like to do?");
-            System.out.println("1 - Add position to the list.");
-            System.out.println("2 - check the list");
-            System.out.println("3 - exit.");
+        while (true) {
+            System.out.println("What would you like to do?");
+            System.out.println("1 - Add position to the list");
+            System.out.println("2 - Check the list");
+            System.out.println("3 - Exit");
             System.out.println();
-            if(scanner.nextInt() == 1) {
-                System.out.println("Add id:");
+            int choice = scanner.nextInt();
+            if (choice == 1) {
+                System.out.println("Add ID:");
                 id = scanner.nextInt();
-                if (map.containsKey(id)){
-                    System.out.println("this key already exists, try another one.");
+                if (map.containsKey(id)) {
+                    System.out.println("This ID already exists, try another one.");
                     continue;
                 }
                 System.out.println("Add Name:");
@@ -34,27 +42,28 @@ public class Zad1 {
                 map.put(id, name);
                 continue;
             }
-            if (scanner.nextInt() == 2) {
-                if (map.isEmpty()){
-                    System.out.println("the map is empty");
+            if (choice == 2) {
+                if (map.isEmpty()) {
+                    System.out.println("The map is empty");
                     continue;
-                }
-                else {
+                } else {
                     System.out.println(map);
                     continue;
                 }
             }
-            if(scanner.nextInt() == 3) {
+            if (choice == 3) {
                 break;
+            }
+            else {
+                System.out.println("This option does not exist.");
             }
 
 
-        }
 
         }
-
-
-
 
     }
+
+
+}
 
