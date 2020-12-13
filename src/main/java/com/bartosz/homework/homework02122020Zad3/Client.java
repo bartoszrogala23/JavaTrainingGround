e5c699ceb04b6f1c41be4bff5a0c8e245e40307c
Bartosz Rogala <bartoszrogala23@gmail.com>
2020-12-13 12:36:42 +0100

    zad 3 praktycznie działa - do oceny Tomka

--- src/main/java/com/bartosz/homework/homework02122020Zad3/Client.java
+++ src/main/java/com/bartosz/homework/homework02122020Zad3/Client.java
@@ -1,8 +1,12 @@
 package com.bartosz.homework.homework02122020Zad3;
 
-public class Client {
-   String clientName;
+import java.util.HashMap;
+import java.util.Map;
 
+public class Client {
+    private String clientName;
+    private int clientID;
+    private Map<Integer, String> clientMap2 = new HashMap<>();
 
     public String getClientName() {
         return clientName;
@@ -10,5 +14,24 @@ public class Client {
 
     public void setClientName(String clientName) {
         this.clientName = clientName;
+    }
+
+    public int getClientID() {
+        return clientID;
+    }
+
+    public void setClientID(int clientID) {
+        this.clientID = clientID;
+    }
+
+    public void setNewClient(String clientName) {
+        this.clientID = clientID=+1;
+        this.clientName = clientName;
+        clientMap2.put(this.clientID, this.clientName);
+    }
+
+
+    public Map<Integer, String> getClientMap2() {
+        return clientMap2;
     }
 }
--- src/main/java/com/bartosz/homework/homework02122020Zad3/ClientProgram.java
+++ src/main/java/com/bartosz/homework/homework02122020Zad3/ClientProgram.java
@@ -21,52 +21,95 @@ package com.bartosz.homework.homework02122020Zad3;
 //
 //        Podpowiedzi - użyj Scanner, pętli While, do obsługi stwórz nową klasę z metodami
 
+
+// propozycja wyboru w formie metody - najpewniej można zrobic metode która posiada atrybuty:
+// int actionNumber (u mnie liczby 1 -7), String message, String choice - scanner.next()
+
+
 import java.util.*;
 
 public class ClientProgram {
 
     public static void main(String[] args) {
         List<String> clientList = new ArrayList<>();
+        Map<Integer, String> clientMap = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
         scanner.useLocale(Locale.US);
         String name;
+        int clientID = 0;
+
+        Client client = new Client();
 
         while (true) {
             System.out.println("What would you like to do?");
             System.out.println("1 - Add position to the list");
             System.out.println("2 - Check the list");
-            System.out.println("3 - delete from the list");
+            System.out.println("3 - Delete from the list");
             System.out.println("4 - Exit");
+            System.out.println("5 - Check if name is on the list");
+            System.out.println("6 - Count the name occurences");
             System.out.println();
             int choice = scanner.nextInt();
             if (choice == 1) {
                 System.out.println("put name:");
                 name = scanner.next();
-                clientList.add(name);
+                clientMap.put(clientID, name);
+                clientID++;
+
             }
             if (choice == 2) {
-                if (clientList.isEmpty()) {
+                if (clientMap.isEmpty()) {
                     System.out.println("The map is empty");
                     continue;
                 } else {
-                    System.out.println(clientList);
+                    System.out.println(clientMap);
                     continue;
-                }
-            }
-            if (choice == 3) {
-                System.out.println("who you want to remove from the list?");
-                System.out.println(clientList);
-                String deleteName = scanner.next();
-                if (clientList.contains(deleteName)){
-                    clientList.remove(deleteName);
                 }
             }
             if (choice == 4) {
                 break;
             }
-            else {
+            if (choice == 3) {
+                System.out.println("who you want to remove from the list?");
+                System.out.println(clientMap);
+                System.out.println("type ID number of person who you'd like to remove from the list");
+                int deleteName = scanner.nextInt();
+                if (clientMap.containsKey(deleteName)) ;
+                clientMap.remove(deleteName);
+//                nie wiem jak zrobic zapis: "jeśli clientmap nie posiada Key (deleteName){
+//               sout " podany klucz nie isnieje"
+            }
+            if (choice == 5) {
+                System.out.println("put a name to verify: ");
+                String verifyName = scanner.next();
+                if (clientMap.containsValue(verifyName)) {
+                    System.out.println(verifyName + " is on the list.");
+                }
+            }
+            if (choice == 6) {
+                System.out.println("which name occurencies you'd like to count?");
+                String nameToCount = scanner.next();
+                if (clientMap.containsValue(nameToCount)) {
+                    Map<String, Integer> resultMap = new HashMap<String, Integer>();
+                    for (int key : clientMap.keySet()) {
+                        String userName = (String) clientMap.get(nameToCount);
+                        if (resultMap.containsKey(userName)) {
+//                            poniższy zapis znalazłem w necie
+//                            https://stackoverflow.com/questions/44500446/count-occurrences-of-value-in-a-map
+//                            nie do konca rozumiem ale działa.
+                            resultMap.put(userName, resultMap.get(userName) + 1);
+                            System.out.println(nameToCount + " is on the list.");
+                            System.out.println(nameToCount + " occurencies: " + resultMap.get(userName));
+                        } else {
+                            resultMap.put(userName, 1);
+                        }
+                    }
+                }
+            } else {
                 System.out.println("This option does not exist.");
             }
+
         }
+
     }
 }
